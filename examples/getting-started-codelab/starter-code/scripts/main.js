9b64d2fb0f1480fb7bd08a66a969a408975faf7b
Johnny Oshika <johnnyoshika@gmail.com>
2022-04-27 17:49:47 -0700

    Do getting started codelab

--- examples/getting-started-codelab/starter-code/scripts/main.js
+++ examples/getting-started-codelab/starter-code/scripts/main.js
@@ -9,10 +9,19 @@
 
   function handlePlay(event) {
     // Add code for playing sound.
+    loadWorkspace(event.target);
+    let code = Blockly.JavaScript.workspaceToCode(Blockly.common.getMainWorkspace());
+    code += 'MusicMaker.play();';
+    try {
+      eval(code);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   function save(button) {
     // Add code for saving the behavior of a button.
+    button.blocklySave = Blockly.serialization.workspaces.save(Blockly.common.getMainWorkspace());
   }
 
   function handleSave() {
@@ -36,9 +45,18 @@
     });
   }
 
+  function loadWorkspace(button) {
+    const workspace = Blockly.common.getMainWorkspace();
+    if (button.blocklySave)
+      Blockly.serialization.workspaces.load(button.blocklySave, workspace);
+    else 
+      Blockly.serialization.workspaces.load({}, workspace);
+  }
+
   function enableBlocklyMode(e) {
     document.body.setAttribute('mode', 'blockly');
     currentButton = e.target;
+    loadWorkspace(currentButton);
   }
 
   document.querySelector('#edit').addEventListener('click', enableEditMode);
@@ -47,4 +65,32 @@
 
   enableMakerMode();
 
+  const toolbox = {
+    'kind': 'flyoutToolbox',
+    'contents': [
+      {
+        'kind': 'block',
+        'type': 'controls_repeat_ext',
+        'inputs': {
+          'TIMES': {
+            'shadow': {
+              'type': 'math_number',
+              'fields': {
+                'NUM': 5
+              }
+            }
+          }
+        }
+      },
+      {
+        'kind': 'block',
+        'type': 'play_sound'
+      }
+    ]
+  };
+
+  Blockly.inject('blocklyDiv', {
+    toolbox: toolbox,
+    scrollbars: false
+  });
 })();
